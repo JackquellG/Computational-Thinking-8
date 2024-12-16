4b9b1cf8a469d5da2ae6a6089d3ac4e20761b92b
JackquellG <jackgardner@seattleacademy.org>
2024-12-16 20:16:17 +0000

    project2

---
+++ Projects/Project2.py
@@ -0,0 +1,41 @@
+soccer_points = 0
+basketball_points = 0
+
+
+answer = input ("Do you like A) Soccer, or B) basketball")
+if answer == "A":
+    soccer_points += 1
+elif answer == "B":
+    basketball_points += 1
+
+
+
+answer = input ("Would you rather A) Play in the NBA, or B) play in the the MLS")
+if answer == "B":
+    soccer_points += 1
+elif answer == "A":
+    basketball_points += 1
+
+
+
+answer = input ("Which player is better? Option A) Ronaldo, or B) Lebron")
+if answer == "A":
+    soccer_points += 1
+elif answer == "B":
+    basketball_points += 1
+
+
+
+answer = input ("Which player is better? Option A) Pele, or B) Kevin Durant")
+if answer == "A":
+    soccer_points += 1
+elif answer == "B":
+    basketball_points += 1
+
+
+
+answer = input ("Would you rather A) play Fifa25, or B) play 2K25")
+if answer == "A":
+    soccer_points += 1
+elif answer == "B":
+    basketball_points += 1
