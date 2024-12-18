0ac2a15b134b6c985b2a796985b5362a3463ad06
JackquellG <jackgardner@seattleacademy.org>
2024-12-18 18:26:08 +0000

    project2

--- Projects/Project2.py
+++ Projects/Project2.py
@@ -1,8 +1,9 @@
+#start
 soccer_points = 0
 basketball_points = 0
 
-
-answer = input ("Do you like A) Soccer, or B) basketball")
+#middle
+answer = input ("Do you like A) Soccer, or B) basketball?")
 if answer == "A":
     soccer_points += 1
 elif answer == "B":
@@ -10,7 +11,7 @@ elif answer == "B":
 
 
 
-answer = input ("Would you rather A) Play in the NBA, or B) play in the the MLS")
+answer = input ("Would you rather A) Play in the NBA, or B) play in the the champions league?")
 if answer == "B":
     soccer_points += 1
 elif answer == "A":
@@ -18,7 +19,7 @@ elif answer == "A":
 
 
 
-answer = input ("Which player is better? Option A) Ronaldo, or B) Lebron")
+answer = input ("Would you rather? Option A) meet Ronaldo, or B) Lebron?")
 if answer == "A":
     soccer_points += 1
 elif answer == "B":
@@ -26,7 +27,7 @@ elif answer == "B":
 
 
 
-answer = input ("Which player is better? Option A) Pele, or B) Kevin Durant")
+answer = input ("Would you rather A) be outside, or B) be indoors?")
 if answer == "A":
     soccer_points += 1
 elif answer == "B":
@@ -34,8 +35,17 @@ elif answer == "B":
 
 
 
-answer = input ("Would you rather play A) Fifa25, or B) 2K25")
+answer = input ("Would you rather play A) Fifa25, or B) 2K25?")
 if answer == "A":
     soccer_points += 1
 elif answer == "B":
     basketball_points += 1
+
+
+#end
+if soccer_points > basketball_points:
+    print("Your weird cus you like soccer")
+elif basketball_points > soccer_points:
+    print ("Your a pretty chill person cus you know basketball is better")
+elif soccer_points == basketball_points:
+    print ("You think that soccer and basketball are the same you sheep")
