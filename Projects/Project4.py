f38bc43474aaac6359ba30f129d5024e3018fb24
JackquellG <jackgardner@seattleacademy.org>
2025-01-14 18:28:04 +0000

    CodeArt

--- Projects/Project4.py
+++ Projects/Project4.py
@@ -3,13 +3,14 @@ import turtle
 
 t = turtle.Turtle()
 
-
-t.speed(100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
-colors = ["red", "dark red", "pink", "black"]
+t.goto(-50, 35)
+t.speed(10000000000000000000000000000000000000000000000000000000000000000000000000000)
+# colors change every 4th time
+colors = ["red", "dark red", "black"]
 for i in range (800000000000000):
-    t.color(colors[i%4])
-    t.forward(0.1 + i/14)
-    t.left(45 + 1)
+    t.color(colors[i%3])
+    t.forward(100 + i/2)
+    t.left(180 + 3)
 
 #End
 turtle.exitonclick()
