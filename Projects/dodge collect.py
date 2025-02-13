b8f56ef3f017a0c635e19a9b3ce2003afd560a1c
JackquellG <jackgardner@seattleacademy.org>
2025-02-13 18:39:35 +0000

    Jack Gardner and Jack Howard Gregersen Project 4

--- Projects/dodge collect.py
+++ Projects/dodge collect.py
@@ -2,40 +2,51 @@
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
-stage.disable_floor()
+stage.disable_all_walls()
 object_speed = 5
 score = 0
+player = codesters.Sprite("lebron")
+stage.set_background("court")
+
+
+########################################
+
 
 # Section 2 - Objects
-player = codesters.Sprite("lebron")
-object = codesters.Sprite("trophy")
-stage.set_background("court")
 def falling_object():
     global object_speed, score
     
     if score < 100:
         y = random.randint(-50,50)
-        object.set_x_speed = 5
         x = 400
         object = codesters.Sprite("trophy", x, y)
-        object.set_size(0.5)
+        object.set_x_speed(-5)
+        object.set_size(0.1)
+        player.set_size(0.2)
 
 stage.event_interval(falling_object, 2)
         
 
+########################################
+
+
 # Section 3 - Collision
 def collision(player, object):
-    global score
+    global score,object_speed
 
     if object.get_image_name() == "trophy":
         stage.remove_sprite(object)
         object_speed += 1
+        score += 1
         if score == 100:
-            player.say(f"You are now lore-accurate lebron!")
-        else:
+            player.say(f"You are now lore-accurate lebron!",5)
+        elif score < 100:
             player.say(f"{score} champions",0.5)
         
 player.event_collision(collision)
+
+
+########################################
 
 
 # Section 4 - Controls
@@ -43,10 +54,18 @@ player.event_collision(collision)
 # Right Key
 def go_up():
     player.move_up(10)
-
 player.event_key("up", go_up)
 
+# Left Key
 def go_down():
     player.move_down(10)
-
 player.event_key("down", go_down)
+
+
+########################################
+
+########################################
+
+########################################
+
+# you did it! #
